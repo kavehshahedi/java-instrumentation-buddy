7685c8f5089c5fd0a6b8b38201fb08cda4f04832
Kaveh Shahedi <kavehshahedi@gmail.com>
2024-05-15 16:36:43 -0400

    [INITIAL] Agent with asnyc file writing

---
+++ src/main/java/ca/polymtl/gigl/moose/kavehshahedi/bbi/FunctionEntryAgent.java
@@ -0,0 +1,96 @@
+package ca.polymtl.gigl.moose.kavehshahedi.bbi;
+
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.utility.JavaModule;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+public class FunctionEntryAgent {
+
+    public static void premain(String args, Instrumentation inst) throws SecurityException, IOException {
+        clearLogFile();
+
+        String targetPackage = args;
+        
+        new AgentBuilder.Default()
+                .type(ElementMatchers.nameStartsWith(targetPackage))
+                .transform(new AgentBuilder.Transformer.ForAdvice()
+                        .advice(ElementMatchers.isMethod(),
+                                MethodExecutionTime.class.getName()))
+                .installOn(inst);
+    }
+
+    public static class MethodExecutionTime {
+        private static final String LOG_FILE = "method_logs.log";
+
+        @Advice.OnMethodEnter
+        static void enter(@Advice.Origin Method method) {
+            logTime(method, "ENTER");
+        }
+
+        @Advice.OnMethodExit
+        static void exit(@Advice.Origin Method method) {
+            logTime(method, "EXIT");
+        }
+
+        public static void logTime(Method method, String type) {
+            StringBuilder messageBuilder = new StringBuilder();
+            messageBuilder.append("[").append(System.nanoTime()).append("] ")
+                    .append(type).append(" ")
+                    .append(method.getDeclaringClass().getName()).append(".").append(method.getName()).append(" ")
+                    .append("(").append(Arrays.toString(method.getParameterTypes())).append(") ")
+                    .append(method.getReturnType().getName());
+
+            CompletableFuture.runAsync(() -> writeLog(messageBuilder.toString()));
+            // writeLog(messageBuilder.toString()); // If the java version for compiling is 1.6 or 1.7, use this line instead of the above line
+        }
+
+        public static class LoggerSupplier implements Supplier<Void> {
+            private final String message;
+
+            public LoggerSupplier(String message) {
+                this.message = message;
+            }
+
+            @Override
+            public Void get() {
+                try (Writer writer = new BufferedWriter(new FileWriter(LOG_FILE, true))) {
+                    writer.write(message + "\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+        }
+    }
+
+    public static void writeLog(String message) {
+        try (Writer writer = new BufferedWriter(new FileWriter(MethodExecutionTime.LOG_FILE, true))) {
+            writer.write(message + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void clearLogFile() {
+        // Remove the log file if it exists from the system
+        File file = new File("method_logs.log");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+}
---
+++ trace_formatter.py
@@ -0,0 +1,35 @@
+import json
+import os
+import re
+
+with open('method_logs.log', 'r') as f:
+    data = f.readlines()
+
+output = []
+for line in data:
+    # Make a regex with the pattern of the data
+    # This regex will be used to extract the duration, method name, method params, and method return type
+    # The format is [duration] method_name (method_params) method_return_type
+    # Example: [29300] ENTER|EXIT com.package.class.method (params) int
+    regex = re.compile(r'\[(\d+)\] (ENTER|EXIT) ([\w\W.]+) \(([\w\W, ]*)\) ([\w\W]+)')
+    match = regex.match(line)
+
+    # If the regex matches the line, extract the data
+    if match:
+        timestamp = int(match.group(1))
+        eventType = match.group(2)
+        method_name = match.group(3)
+        method_params = match.group(4)
+        method_return_type = match.group(5).strip()
+
+        output.append({
+            'ts': timestamp,
+            'ph': 'B' if eventType == 'ENTER' else 'E',
+            'name': f'{method_name}({method_params}) {method_return_type}'
+        })
+    else:
+        print(f'No match: {line}')
+
+# Write the sorted methods to a JSON file
+with open('trace.json', 'w') as f:
+    json.dump(output, f, indent=4)
