1843324f9cb97ce41b10c51ce5c45d8b0fa04a80
Kaveh Shahedi <kavehshahedi@gmail.com>
2024-08-05 11:38:02 -0400

    [FEATURE] Track agent's start and end time

--- src/main/java/kavehshahedi/jib/JavaInstrumentationBuddy.java
+++ src/main/java/kavehshahedi/jib/JavaInstrumentationBuddy.java
@@ -9,6 +9,7 @@ import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.matcher.ElementMatcher;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.instrument.Instrumentation;
@@ -22,19 +23,13 @@ import org.yaml.snakeyaml.constructor.Constructor;
 import kavehshahedi.jib.models.Configuration;
 
 import kavehshahedi.jib.services.Logger;
+import kavehshahedi.jib.services.Time;
 
 public class JavaInstrumentationBuddy {
 
     public static Logger logger;
 
     public static Configuration config = new Configuration();
-
-    public static final long TIME_OFFSET;
-    static {
-        long currentTimeMillis = System.currentTimeMillis();
-        long currentNanoTime = System.nanoTime();
-        TIME_OFFSET = (currentTimeMillis * 1_000_000) - currentNanoTime;
-    }
 
     private JavaInstrumentationBuddy() {
     }
@@ -68,8 +63,10 @@ public class JavaInstrumentationBuddy {
 
         logger = new Logger(config.getLogging());
 
-        ElementMatcher.Junction<TypeDescription> targetPackageMatcher = MethodMatcherHelper.createTargetPackageMatcher(config.getInstrumentation());
-        ElementMatcher.Junction<MethodDescription> methodMatcher = MethodMatcherHelper.createMethodMatcher(config.getInstrumentation());
+        ElementMatcher.Junction<TypeDescription> targetPackageMatcher = MethodMatcherHelper
+                .createTargetPackageMatcher(config.getInstrumentation());
+        ElementMatcher.Junction<MethodDescription> methodMatcher = MethodMatcherHelper
+                .createMethodMatcher(config.getInstrumentation());
 
         System.out.println(config);
 
@@ -79,7 +76,32 @@ public class JavaInstrumentationBuddy {
                         .advice(methodMatcher, MethodExecutionTime.class.getName()))
                 .installOn(inst);
 
+        trackAgentTime();
+
         System.out.println("-----------------------");
+    }
+
+    public static void trackAgentTime() {
+        /*
+         * This method is used to track the start and end time of the agent.
+         */
+        long startTime = Time.getTimeNanoSeconds();
+
+        // Add a shutdown hook to track the end time of the agent.
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                long endTime = Time.getTimeNanoSeconds();
+
+                String info = String.format("{\"start_time\": %d, \"end_time\": %d}",
+                        startTime, endTime);
+
+                try (FileWriter fileWriter = new FileWriter(config.getLogging().getFile().replace(".log", ".json"))) {
+                    fileWriter.write(info);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public static class MethodExecutionTime {
